d7ad53e676c749f2504721904a62fe6a9f29403e
zuiwuchang <zuiwuchang@gmail.com>
2026-04-03 16:50:22 +0800

    env

---
+++ env.go
@@ -0,0 +1,19 @@
+package gosdk
+
+// The root path of the running project
+var Dir string
+
+// Original startup parameters
+var Args []string
+
+// Yaegi version information
+var Yaegi string
+
+// Host version
+var AppVersion string
+
+// Host build version
+var AppDate string
+
+// Host git commit
+var AppCommit string
